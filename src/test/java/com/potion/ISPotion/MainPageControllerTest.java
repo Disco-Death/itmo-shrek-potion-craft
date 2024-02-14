7c742669f3f992297740a0512293b2b8e7333bfb
Disco-Death <muhtarullin450@gmail.com>
2024-02-14 17:09:23 +0300

    Восстанволен доступ к задачам

--- src/test/java/com/potion/ISPotion/MainPageControllerTest.java
+++ src/test/java/com/potion/ISPotion/MainPageControllerTest.java
@@ -1,11 +1,19 @@
 package com.potion.ISPotion;
 
+import com.potion.ISPotion.Classes.Role;
+import com.potion.ISPotion.Classes.User;
 import com.potion.ISPotion.Controllers.MainPageController;
+import com.potion.ISPotion.repo.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashSet;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -14,11 +22,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MainPageControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     public void testHomeMethod() throws Exception {
+        var user = new User();
+        user.setUsername("Test username");
+        var userRoles = new HashSet<Role>();
+        userRoles.add(Role.EMPLOYEE);
+        user.setRoles(userRoles);
+
+        when(userRepository.findByUsername(anyString())).thenReturn(user);
+
         mockMvc.perform(get("/")
-                        .with(user("username")))
+                .with(user(user.getUsername()).roles(user.getRoles().toString())))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
                 .andExpect(model().attributeExists("title"));
